ed7951ffa7d6c466200794f571c87d884464b2fe
MamnuAG <mamun166036@yahoo.com>
2026-04-28 21:16:58 +0600

    chore: update 2 files

--- src/lib/firebase.provider.ts
+++ src/lib/firebase.provider.ts
@@ -1,15 +1,62 @@
+// import { Provider } from '@nestjs/common';
+// import * as admin from 'firebase-admin';
+
+// export const FirebaseProvider: Provider = {
+//     provide: 'FIREBASE_ADMIN',
+//     useFactory: () => {
+//         if (!admin.apps.length) {
+//             admin.initializeApp({
+//                 credential: admin.credential.cert({
+//                     projectId: process.env.FIREBASE_PROJECT_ID!,
+//                     clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+//                     privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+//                 }),
+//             });
+//         }
+
+//         return admin;
+//     },
+// };
+
+
 import { Provider } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 
 export const FirebaseProvider: Provider = {
     provide: 'FIREBASE_ADMIN',
     useFactory: () => {
+        const projectId = process.env.FIREBASE_PROJECT_ID;
+        const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+        let privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+        if (!projectId || !clientEmail || !privateKey) {
+            throw new Error('Missing Firebase environment variables');
+        }
+        // privateKey = privateKey.replace(/\\n/g, '\n').replace(/\r/g, '').trim();
+        privateKey = privateKey
+            .replace(/^["']|["']$/g, '')
+            .replace(/\\\\n/g, '\n')
+            .replace(/\\n/g, '\n')
+            .replace(/\\\r?\n/g, '\n')
+            .replace(/\r/g, '')
+            .trim();
+
+        if (!privateKey.startsWith('-----BEGIN PRIVATE KEY-----')) {
+            throw new Error('Firebase private key does not start correctly');
+        }
+
+        if (!privateKey.endsWith('-----END PRIVATE KEY-----')) {
+            throw new Error('Firebase private key does not end correctly');
+        }
+
+        // console.log(privateKey)
+
         if (!admin.apps.length) {
             admin.initializeApp({
                 credential: admin.credential.cert({
-                    projectId: process.env.FIREBASE_PROJECT_ID!,
-                    clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-                    privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+                    projectId,
+                    clientEmail,
+                    privateKey,
                 }),
             });
         }
@@ -17,3 +64,4 @@ export const FirebaseProvider: Provider = {
         return admin;
     },
 };
+
